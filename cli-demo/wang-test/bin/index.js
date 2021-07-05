fbea2c7dda72f2b470540357f53f9a611dbb4841
wangruibin <1099690237@qq.com>
2021-07-05 21:46:36 +0800

    cli demo

--- cli-demo/wang-test/bin/index.js
+++ cli-demo/wang-test/bin/index.js
@@ -1,5 +1,34 @@
 #!/usr/bin/env node
 
 const lib = require('wang-test-lib');
-console.log(lib)
-console.log('hellow wang!!!!')
+
+// 注册一个命令 wang-test init --name haha
+// console.log(require('process'));
+const argv = require('process').argv;
+
+const command = argv[2];
+
+const options = argv.slice(3);
+
+console.log(options);
+
+let [option, param] = options;
+
+option = option.replace('--', '');
+
+// console.log(option,param);
+
+if(command){
+  if(lib[command]) {
+    lib[command]({option,param});
+  }else{
+    console.log('无效的命令！');
+  }
+}else{
+  console.log('请输入命令！');
+}
+
+
+// console.log(argv[2])
+// console.log(lib);
+// console.log('hellow wang!!!!')
